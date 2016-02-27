a6abec638cb645ee00e1808f867361cc54fc07c9
Topbit Du <topbit.du@gmail.com>
2016-02-27 09:00:36 +0800

    1, Improved the Categorizing model for coding style.

--- app/models/unidom/category/categorizing.rb
+++ app/models/unidom/category/categorizing.rb
@@ -10,6 +10,6 @@ class Unidom::Category::Categorizing < ::ActiveRecord::Base
   scope :category_is,    ->(category)    { where category_id: (category.respond_to?(:id) ? category.id : category) }
   scope :categorized_is, ->(categorized) { where categorized: categorized }
 
-  include ::Unidom::Common::Concerns::ModelExtension
+  include Unidom::Common::Concerns::ModelExtension
 
 end
