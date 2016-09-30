1651f669fb6f865273a3512626eb7f67a0917dc6
Topbit Du <topbit.du@gmail.com>
2016-09-30 10:22:56 +0800

    1, Improve the Category model to include the As Ancestor Category concern & the As Descendant Category concern.

--- app/models/unidom/category/category.rb
+++ app/models/unidom/category/category.rb
@@ -6,17 +6,13 @@ class Unidom::Category::Category < ActiveRecord::Base
 
   include Unidom::Common::Concerns::ModelExtension
   include Unidom::Category::Concerns::AsCategory
+  include Unidom::Category::Concerns::AsAncestorCategory
+  include Unidom::Category::Concerns::AsDescendantCategory
 
   validates :name,         presence:    true, length: { in: 2..self.columns_hash['name'].limit         }
   validates :abbreviation, allow_blank: true, length: { in: 2..self.columns_hash['abbreviation'].limit }
 
   belongs_to :scheme, class_name: 'Unidom::Category::CategoryScheme', foreign_key: :scheme_id
-
-  has_many :ancestor_category_rollups, class_name: 'Unidom::Category::CategoryRollup', foreign_key: :descendant_category_id, source: :descendant_category
-  has_many :ancestor_categories,       class_name: 'Unidom::Category::Category',       through:     :ancestor_category_rollups
-
-  has_many :descendant_category_rollups, class_name: 'Unidom::Category::CategoryRollup', foreign_key: :ancestor_category_id, source: :ancestor_category
-  has_many :descendant_categories,       class_name: 'Unidom::Category::Category',       through:     :descendant_category_rollups
 
   scope :scheme_is,          ->(scheme) { where scheme_id: (scheme.respond_to?(:id) ? scheme.id : scheme )    }
   scope :code_length_is,     ->(length) { where 'LENGTH(code) = :code_length',  code_length:       length     }
