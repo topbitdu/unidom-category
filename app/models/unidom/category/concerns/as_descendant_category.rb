8d0c0f5ca7d2b89d635a55c874d807b13578b2c3
Topbit Du <topbit.du@gmail.com>
2016-10-08 10:56:13 +0800

    1, Improve the As Descendant Category concern to add the #is_rolled_up! method.

--- app/models/unidom/category/concerns/as_descendant_category.rb
+++ app/models/unidom/category/concerns/as_descendant_category.rb
@@ -7,6 +7,17 @@ module Unidom::Category::Concerns::AsDescendantCategory
     has_many :ancestor_category_rollups, class_name: 'Unidom::Category::CategoryRollup', foreign_key: :descendant_category_id, source: :descendant_category
     has_many :ancestor_categories,       class_name: 'Unidom::Category::Category',       through:     :ancestor_category_rollups
 
+    def is_rolled_up!(it, at: Time.now, primary: true)
+      rollup = ancestor_category_rollups.ancestor_category_is(it).valid_at(now: at).alive.first_or_create! elemental: primary, opened_at: at
+      rollup.elemental = primary if rollup.elemental!=primary
+      rollup.save!
+      rollup
+    end
+
+    #def is_rolled_up?(it, at: Time.now, primary: true)
+    #  ancestor_category_rollups.ancestor_category_is(it).primary(primary).valid_at(now: at).alive.exists?
+    #end
+
   end
 
   module ClassMethods
