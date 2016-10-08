9a3856a13ab053b4b9e7870f529b94f31a74034c
Topbit Du <topbit.du@gmail.com>
2016-10-08 09:53:29 +0800

    1, Improve the As Ancestor Category concern to add the #roll_up! method.

--- app/models/unidom/category/concerns/as_ancestor_category.rb
+++ app/models/unidom/category/concerns/as_ancestor_category.rb
@@ -7,6 +7,17 @@ module Unidom::Category::Concerns::AsAncestorCategory
     has_many :descendant_category_rollups, class_name: 'Unidom::Category::CategoryRollup', foreign_key: :ancestor_category_id, source: :ancestor_category
     has_many :descendant_categories,       class_name: 'Unidom::Category::Category',       through:     :descendant_category_rollups
 
+    def roll_up!(it, at: Time.now, primary: true)
+      rollup = descendant_category_rollups.descendant_category_is(it).valid_at(now: at).alive.first_or_create! elemental: primary, opened_at: at
+      rollup.elemental = primary if rollup.elemental!=primary
+      rollup.save!
+      rollup
+    end
+
+    #def roll_up?(it, at: Time.now, primary: true)
+    #  descendant_category_rollups.descendant_category_is(it).primary(primary).valid_at(now: at).alive.exists?
+    #end
+
   end
 
   module ClassMethods
