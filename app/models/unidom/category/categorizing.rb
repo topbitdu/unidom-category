0825cf3587763705b44c0f68c8e412d919d70e1e
Topbit Du <topbit.du@gmail.com>
2017-01-14 10:21:41 +0800

    1, Improve the Categorizing model to remove the un-used source codes.

--- app/models/unidom/category/categorizing.rb
+++ app/models/unidom/category/categorizing.rb
@@ -13,10 +13,6 @@ class Unidom::Category::Categorizing < Unidom::Category::ApplicationRecord
   scope :category_is,    ->(category)    { where category_id: to_id(category) }
   scope :categorized_is, ->(categorized) { where categorized: categorized     }
 
-  #def self.categorize!(category, categorized, opened_at = Time.now)
-  #  self.categorized_is(categorized).category_is(category).valid_at.alive.first_or_create! elemental: true, opened_at: opened_at
-  #end
-
   def self.categorize!(categorized, into: nil, at: Time.now)
     categorized_is(categorized).category_is(into).valid_at.alive.first_or_create! elemental: true, opened_at: at
   end
