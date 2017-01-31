3f51aa166a9e40d48c42beaa2a50779bb296a866
Topbit Du <topbit.du@gmail.com>
2017-01-31 18:55:31 +0800

    1, Models RSpec examples manifest.

---
+++ lib/unidom/category/models_rspec.rb
@@ -0,0 +1,4 @@
+require 'rspec/models/unidom/category/categorizing_spec'    unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Category::Categorizing'
+require 'rspec/models/unidom/category/category_rollup_spec' unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Category::CategoryRollup'
+require 'rspec/models/unidom/category/category_scheme_spec' unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Category::CategoryScheme'
+require 'rspec/models/unidom/category/category_spec'        unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Category::Category'
